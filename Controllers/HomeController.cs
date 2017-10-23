93975cc83eac2acf990518e0390e60f52237467d
pasaunders <kaorti@gmail.com>
2017-10-23 14:34:23 -0700

    display datetime

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace timeDisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            String storingTime = DateTime.Now.ToString("MMMM d, yyyy hh:mm");
+            ViewData["Time"] = storingTime;
+            return View();
+        }
+    }
+}
